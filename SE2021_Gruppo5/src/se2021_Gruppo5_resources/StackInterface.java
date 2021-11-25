e29b1502c56beb574017544383524666b10f80fd
marco <marco@marco-Lenovo>
2021-11-25 23:36:06 +0100

    Modificato ComplexStack eliminato ciò che non serviva

---
+++ SE2021_Gruppo5/src/se2021_Gruppo5_resources/StackInterface.java
@@ -0,0 +1,49 @@
+/*
+ * StackInterface
+ *
+ */
+package se2021_Gruppo5_resources;
+
+/**
+ * Interfaccia per la realizzazione di uno stack.
+ * 
+ * @author roberto
+ */
+public interface StackInterface<Object> {
+    
+    /**
+     * Testa se lo stack è vuoto
+     * 
+     * @return <code>true</code> se lo stack è vuota
+     */
+    boolean isEmpty();
+    
+    /**
+     * Ritorna la dimensione dello stack.
+     * 
+     * @return 
+     */
+    int size();
+    
+    /**
+     * Inserisce un elemento nello stack
+     * 
+     * @param item oggetto da inserire nello stack
+     */
+    void push(Object item);
+    
+    /**
+     * Rimuove il primo elemento dallo stack e lo restituisce
+     * 
+     * @return primo elemento dello stack
+     */
+    Object pop();
+    
+    /**
+     * Restituisce il primo elemento dello stack senza eliminarlo.
+     * 
+     * @return il primo elemento dello stack
+     */
+    Object peek();    
+    
+}
