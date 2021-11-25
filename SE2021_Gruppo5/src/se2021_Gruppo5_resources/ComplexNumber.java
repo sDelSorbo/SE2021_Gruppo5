f3f48cc95548d6e319d3df99db95d3539cbc2294
gianpaolotobia <gianpaolotobia@mbpdigianpaolo.homenet.telecomitalia.it>
2021-11-25 12:38:03 +0100

    Adding of JavaDoc and updated ComplexNumber class and ComplexOperations class

--- SE2021_Gruppo5/src/se2021_Gruppo5_resources/ComplexNumber.java
+++ SE2021_Gruppo5/src/se2021_Gruppo5_resources/ComplexNumber.java
@@ -1,56 +1,146 @@
 package se2021_Gruppo5_resources;
-
+import java.text.DecimalFormat;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
 /**
- *
+ * Questa classe crea un oggetto corrispondente ad un numero complesso
  * @author gianpaolotobia
  */
 public class ComplexNumber {
-    private double re;
-    private double im;
-
-    public ComplexNumber(double re, double im) {
-        this.re = re;
-        this.im = im;
+    private float real; //Parte reale del numero complesso
+    private float imaginary; //Parte immaginaria del numero complesso
+    private String complex; //Stringa contenente il numero complesso con parte reale e parte immaginaria
+    
+    /**
+    * Costruisce un numero complesso, definendo sia la parte reale che la parte immaginaria
+    *
+    *  @param real        parte reale
+    *  @param imaginary   parte immaginaria
+    *  
+    */
+    public ComplexNumber(float real, float imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+        complex = this.toString();
     }
-
-    ComplexNumber() {
+    /**
+    * Costruisce un numero complesso, costituito unicamente dalla parte reale
+    *
+    *  @param real        parte reale
+    *  
+    */
+    public ComplexNumber(float real) {
+        this.real = real;
+        imaginary = 0;
+        complex = this.toString();
     }
     
-    public double getRe() {
-        return re;
+    /**
+    * Costruisce un numero complesso nullo, ovvero con parte reale ed immaginaria pari a 0
+    *  
+    */
+    public ComplexNumber() {
+        real = 0;
+        imaginary = 0;
     }
-
-    public void setRe(double re) {
-        this.re = re;
+    /**
+    * Restituisce la parte reale del numero complesso
+    * 
+    * @return ritorna la parte reale del numero complesso
+    */
+    public float getReal() {
+        return real;
     }
-
-    public double getIm() {
-        return im;
+   /**
+    * Setta la parte reale del numero complesso 
+    * 
+    * @param real parametro che viene settato
+    */
+    public void setReal(float real) {
+        this.real = real;
+        this.setComplex();
     }
-
-    public void setIm(double im) {
-        this.im = im;
+   /**
+    * Restituisce la parte immaginaria del numero complesso
+    * 
+    * @return ritorna la parte immaginaria del numero complesso
+    */
+    public float getImaginary() {
+        return imaginary;
+    }
+    /**
+    * Setta la parte immaginaria del numero complesso 
+    * 
+    * @param imaginary parametro che viene settato
+    */
+    public void setImaginary(float imaginary) {
+        this.imaginary = imaginary;
+        this.setComplex();
+    }
+   /**
+    * Restituisce la stringa rappresentativa del numero complesso
+    * 
+    * @return ritorna la stringa che rappresenta l'intero numero complesso
+    */
+    public String getComplex() {
+        return complex;
+    }
+    /**
+    * Setta la stringa che rappresenta il numero complesso
+    *
+    */
+    public void setComplex() {
+        complex = this.toString();
     }
     
-   public String toString()
-   {
+    
+   //Metodo che ritorna la stringa del numero complesso
+   public String toString(){
+      DecimalFormat df = new DecimalFormat("###.#####"); //Formato della stringa da salvare in complex
+      
       final String UNITA_IMMAGINARIA = "j";
       String sign = "";
-      if (im==0)
-         return String.valueOf(re);
+      if (imaginary==0)
+         return String.valueOf(real);
 
-      if (re==0)
-         return String.valueOf(im) + UNITA_IMMAGINARIA;
+      if (real==0)
+         return String.valueOf(imaginary) + UNITA_IMMAGINARIA;
          
-      if (im > 0)
+      if (imaginary > 0)
          sign = "+";
          
-      return re + sign + im + UNITA_IMMAGINARIA;
+      return df.format(real) + sign + df.format(imaginary) + UNITA_IMMAGINARIA;
    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+    //Metodo per verificare se due ComplexNumber sono uguali
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ComplexNumber other = (ComplexNumber) obj;
+        if (Float.floatToIntBits(this.real) != Float.floatToIntBits(other.real)) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.imaginary) != Float.floatToIntBits(other.imaginary)) {
+            return false;
+        }
+        return true;
+    }
+
     
 }
--- SE2021_Gruppo5/src/se2021_Gruppo5_resources/ComplexOperations.java
+++ SE2021_Gruppo5/src/se2021_Gruppo5_resources/ComplexOperations.java
@@ -18,63 +18,67 @@ public class ComplexOperations {
 
     public ComplexOperations() {
     }
-
+    
+    //metodo statico che restituisce la somma di due numeri complessi
     static public ComplexNumber add(ComplexNumber op1, ComplexNumber op2){
         
-        return new ComplexNumber(op1.getRe()+op2.getRe(), op1.getIm()+op2.getIm());
+        return new ComplexNumber((op1.getReal()+op2.getReal()),(op1.getImaginary()+op2.getImaginary()));
         
     }
-    
+    //metodo statico che restituisce la differenza tra due numeri complessi
     static public ComplexNumber sub(ComplexNumber op1, ComplexNumber op2){
    
-        return new ComplexNumber(op1.getRe()-op2.getRe(), op1.getIm()-op2.getIm());
+        return new ComplexNumber((op1.getReal()-op2.getReal()),(op1.getImaginary()-op2.getImaginary()));
     }
-    
+    //metodo statico che restituisce il prodotto tra due numeri complessi
     static public ComplexNumber mul(ComplexNumber op1, ComplexNumber op2){
    
-        return new ComplexNumber((op1.getRe()*op2.getRe())-(op1.getIm()*op2.getIm()),(op1.getRe()*op2.getIm())+(op1.getIm()*op2.getRe()));
+        return new ComplexNumber(((op1.getReal()*op2.getReal())-(op1.getImaginary()*op2.getImaginary())),((op1.getReal()*op2.getImaginary())+(op1.getImaginary()*op2.getReal())));
     }
-    
+    //metodo statico che effettua la divisione tra due numeri complessi
     static public ComplexNumber div(ComplexNumber op1, ComplexNumber op2){
-        if(op2.getRe()==0&& op2.getIm()==0)
-            throw new InvalidOperationException("Divisione per 0");
-        return mul(op1,inv(op2));
+        
+        if(op2.equals(new ComplexNumber())) //Verifico se il secondo operando è uguale a 0
+            throw new ArithmeticException("Divisione per 0"); // Se il secondo operando è uguale a 0, viene eseguita l'eccezzione
+        return mul(op1,inv(op2)); //Ritorna il prodotto fra il primo operando e l'inverso del secondo operando
         
     }
-    
+    //Metodo statico che ritorna il modulo di un numero complesso
     static public double mod(ComplexNumber op){
         
-        return Math.sqrt(op.getRe()*op.getRe()+op.getIm()*op.getIm());
+        return Math.sqrt(op.getReal()*op.getReal()+op.getImaginary()*op.getImaginary()); //Ritorna il modulo effettuando la radice quadrata della somma dei quadrati di parte reale e parte immaginaria
     }
+    //Metodo statico che ritorna l'inverso di un numero complesso
     static public ComplexNumber inv(ComplexNumber op){
         
-        return new ComplexNumber(op.getRe()/(mod(op)*mod(op)),-op.getIm()/(mod(op)*mod(op)));
+        return new ComplexNumber((float)(op.getReal()/(mod(op)*mod(op))),(float)(-op.getImaginary()/(mod(op)*mod(op)))); //Restituisce l'inverso calcolando un numero complesso la cui parte reale è la parte reale diviso il modulo quadro e la parte immaginaria è data dalla parte immaginaria diviso il modulo quadro
         
     }
-    
+    //Metodo statico che inverte il segno di un numero complesso
     static public ComplexNumber sign_inv(ComplexNumber op){
         
-        return new ComplexNumber(-op.getRe(),-op.getIm());
+        return new ComplexNumber(-op.getReal(),-op.getImaginary());
 
     }
-    
+    //Metodo statico che ritorna l'argomento di un numero complesso
     static public double arg(ComplexNumber op){
         
-        return Math.atan(op.getIm()/op.getRe());  
+        return Math.atan(op.getImaginary()/op.getReal());  
 
     }
+    //Metodo statico che ritorna una lista delle radici quadrate di un numero complesso
     static public LinkedList sqrt_c(ComplexNumber op){
-        double argument = arg(op);
-        double module = mod(op);
-        LinkedList<ComplexNumber> radici = new LinkedList<>();
+        double argument = arg(op); //Definisco l'argomento dell'operando
+        double module = mod(op); //Definisco il modulo dell'operando
+        LinkedList<ComplexNumber> squares = new LinkedList<>(); //Definisco una lista di numeri complessi
         
         for(int k=0;k<2;k++){
-        ComplexNumber rad = new ComplexNumber();
-        rad.setRe(Math.sqrt(module)*(Math.cos((argument + (2*k*Math.PI))/2)));
-        rad.setIm(Math.sqrt(module)*(Math.sin((argument + (2*k*Math.PI))/2)));
-        radici.add(rad);
+        ComplexNumber sqr = new ComplexNumber();
+        sqr.setReal((float) (Math.sqrt(module)*(Math.cos((argument + (2*k*Math.PI))/2)))); //Calcolo la parte reale della radice
+        sqr.setImaginary((float) (Math.sqrt(module)*(Math.sin((argument + (2*k*Math.PI))/2)))); //Calcolo la parte immaginari della radice
+        squares.add(sqr);//Aggiungo il numero complesso alla lista di radici e ripeto il ciclo per k=1
         }
-        return radici;
+        return squares;
     }
     
 
