05cbb8dacde885244763a29756131bf965a457a3
abc <abc@DESKTOP-BTRMKI3>
2021-12-01 18:34:39 +0100

    Implementazione della classe Variables

---
+++ SE2021_Gruppo5/src/it/unisa/diem/se/group5/calculator/complex/Variables.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.se.group5.calculator.complex;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author abc
+ */
+public class Variables {
+    
+    private static Map<String,ComplexNumber> variablesMap;
+ 
+    public Variables() {
+        variablesMap = new HashMap<String,ComplexNumber>();
+        String output = "";
+        for(char alphabet = 'a'; alphabet <='z'; alphabet++ ) {
+            output += alphabet;
+            variablesMap.put(output, new ComplexNumber(0, 0));
+            output = "";
+        }
+    }
+    
+    public void setVariable(String var, ComplexNumber num) throws IllegalArgumentException{
+        if(!variablesMap.containsKey(var)) {
+            throw new IllegalArgumentException("Variable not valid!");
+        }
+        variablesMap.put(var, num);
+    }
+
+    @Override
+    public String toString() {
+        
+        String value = "";
+        
+        for(Map.Entry<String, ComplexNumber> map:variablesMap.entrySet()) {
+            if(map.getValue() == null) {
+                value+="\n" + map.getKey();
+            } else {
+               value+="\n" + map.getKey()+" "+map.getValue(); 
+            }
+        }
+        
+        return value;
+    }
+    
+    
+    
+}
