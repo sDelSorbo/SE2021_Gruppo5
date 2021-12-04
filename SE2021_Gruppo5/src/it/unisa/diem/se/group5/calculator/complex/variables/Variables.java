61ec2d5e66422600e3027e1fa00dde1a68387793
abc <abc@DESKTOP-BTRMKI3>
2021-12-04 16:11:34 +0100

    Classe Variables aggiornata

--- SE2021_Gruppo5/src/it/unisa/diem/se/group5/calculator/complex/variables/Variables.java
+++ SE2021_Gruppo5/src/it/unisa/diem/se/group5/calculator/complex/variables/Variables.java
@@ -1,50 +1,80 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Variables
  */
 package it.unisa.diem.se.group5.calculator.complex.variables;
 
-import it.unisa.diem.se.group5.calculator.complex.ComplexNumber;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
- * @author abc
+ * Questa classe implementa un HashMap con 26 variabili 
+ * che vanno dalla "a" alla "z" inizializzate a 0.
+ * 
+ * @author roberto
  */
 public class Variables {
-    
-    private static Map<String,ComplexNumber> variablesMap; 
+    /**
+     * Struttura dati Map che conterrà le 26 variabili.
+     */
+    private static Map<String, ComplexNumber> variablesMap; 
  
+    /**
+     * Costruttore della classe Variables che inizializza le 26 variabili a 0.
+     */
     public Variables() {
         variablesMap = new HashMap<>();
-        for(char alphabet = 'a'; alphabet <='z'; alphabet++ ) {
+        for(char alphabet = 'a'; alphabet <= 'z'; alphabet++) {
             variablesMap.put(String.valueOf(alphabet), new ComplexNumber(0, 0));
         }
     }
     
+    /**
+     * Restituisce la Map.
+     * @return Map
+     */
     public Map<String, ComplexNumber> getVariablesMap() {
         return variablesMap;
     }
     
+    /**
+     * Data una chiave e un numero comlesso setta il valore associato alla chiave con il nuovo numero complesso.
+     * @param var chiave dell'HashMap
+     * @param num numero complesso da settare
+     * @throws IllegalArgumentException se si passa una chiave non valida
+     */
     public void setVariable(String var, ComplexNumber num) throws IllegalArgumentException{
-        if(!variablesMap.containsKey(var)) {
+        if(!variablesMap.containsKey(var) || var == null || num == null) {
             throw new IllegalArgumentException("Variable not valid!");
         }
         variablesMap.put(var, num);
     }
+    
+    /**
+     * Ritorna il numero complesso associato alla chiave.
+     * @param var chiave dell'HashMap
+     * @return il numero complesso associato alla chiave
+     * @throws IllegalArgumentException se si passa una chiave non valida
+     */
+    public ComplexNumber getValue(String var) throws IllegalArgumentException{
+        if(!variablesMap.containsKey(var) || var == null ) {
+            throw new IllegalArgumentException("Variable not valid!");
+        }
+        return variablesMap.get(var);
+    }
 
+    /**
+     * Ritorna una stringa che rappresenta l' HashMap.
+     * @return la sequenza di elementi visti come coppia chiave-valore contenuti nell'HashMap 
+     */
     @Override
     public String toString() {
-        
         String value = "";
         
-        for(Map.Entry<String, ComplexNumber> map:variablesMap.entrySet()) {
+        for(Map.Entry<String, ComplexNumber> map : variablesMap.entrySet()) {
             if(map.getValue() == null) {
-                value+="\n" + map.getKey();
+                value += "\n" + map.getKey();
             } else {
-               value+="\n" + map.getKey()+" "+map.getValue(); 
+               value += "\n" + map.getKey() + " " + map.getValue(); 
             }
         }
         
