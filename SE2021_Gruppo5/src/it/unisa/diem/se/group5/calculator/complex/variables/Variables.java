4e87dbe150e470f98bca985c146c0175ce29f76d
abc <abc@DESKTOP-BTRMKI3>
2021-12-04 17:28:43 +0100

    Test dei metodi dello stack

--- SE2021_Gruppo5/src/it/unisa/diem/se/group5/calculator/complex/variables/Variables.java
+++ SE2021_Gruppo5/src/it/unisa/diem/se/group5/calculator/complex/variables/Variables.java
@@ -4,8 +4,12 @@
 package it.unisa.diem.se.group5.calculator.complex.variables;
 
 import it.unisa.diem.se.group5.calculator.complex.ComplexNumber;
+import it.unisa.diem.se.group5.calculator.complex.commonoperations.Add;
+import it.unisa.diem.se.group5.calculator.complex.commonoperations.Operation;
+import it.unisa.diem.se.group5.calculator.complex.commonoperations.Sub;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * Questa classe implementa un HashMap con 26 variabili 
@@ -25,7 +29,7 @@ public class Variables {
     public Variables() {
         variablesMap = new HashMap<>();
         for(char alphabet = 'a'; alphabet <= 'z'; alphabet++) {
-            variablesMap.put(String.valueOf(alphabet), new ComplexNumber(0, 0));
+            variablesMap.put(String.valueOf(alphabet), null);
         }
     }
     
@@ -43,9 +47,9 @@ public class Variables {
      * @param num numero complesso da settare
      * @throws IllegalArgumentException se si passa una chiave non valida
      */
-    public void setVariable(String var, ComplexNumber num) throws IllegalArgumentException{
+    private void setVariable(String var, ComplexNumber num) throws IllegalArgumentException{
         if(!variablesMap.containsKey(var) || var == null || num == null) {
-            throw new IllegalArgumentException("Variable not valid!");
+            throw new IllegalArgumentException("Variabile not valida");
         }
         variablesMap.put(var, num);
     }
@@ -56,13 +60,88 @@ public class Variables {
      * @return il numero complesso associato alla chiave
      * @throws IllegalArgumentException se si passa una chiave non valida
      */
-    public ComplexNumber getValue(String var) throws IllegalArgumentException{
+    private ComplexNumber getValue(String var) throws IllegalArgumentException{
         if(!variablesMap.containsKey(var) || var == null ) {
-            throw new IllegalArgumentException("Variable not valid!");
+            throw new IllegalArgumentException("Variabile non valida");
         }
         return variablesMap.get(var);
     }
-
+    
+    /**
+     * Metodo statico per caricare il numero complesso in cima allo stack in una variabile
+     * 
+     * @param stack Stack di numeri complessi
+     * @param var Variable nella quale caricare il valore 
+     */
+    public void variableLoad(Stack<ComplexNumber> stack, String var) throws NotSelectedVariableException{
+        if(var==null)
+            throw new NotSelectedVariableException();
+        ComplexNumber n = stack.peek();
+        variablesMap.replace(var, n);
+    }
+    
+    /**
+     * Metodo statico per salvare il valore di una variabile in cima allo stack
+     * 
+     * @param stack Stack di numeri complessi
+     * @param var Variabile il cui valore deve essere salvato nello stack
+     */
+    public void variableSave(Stack<ComplexNumber> stack, String var) throws NotSelectedVariableException{
+        if(var==null)
+            throw new NotSelectedVariableException();
+        if(variablesMap.get(var) == null)
+            throw new NotSelectedVariableException("La variabile non contiene alcun valore");
+        stack.push(variablesMap.get(var));
+        
+    }
+    
+    /**
+     * Metodo statico per sommare il valore della variabile data con il valore del numero complesso
+     * in cima allo stack
+     * 
+     * @param stack Stack di numeri complessi
+     * @param var Variabile il cui valore deve essere sommato al numero complesso in cima allo stack
+     */
+    public void variableAdd(Stack<ComplexNumber> stack, String var) throws NotSelectedVariableException{
+        if(var==null)
+           throw new NotSelectedVariableException();
+        if(variablesMap.get(var) == null)
+            throw new NotSelectedVariableException("La variabile non contiene alcun valore.\nInizializzarla prima");
+        
+        Stack<ComplexNumber> tmp = new Stack<>();
+        tmp.push(stack.peek());
+        tmp.push(variablesMap.get(var));
+        
+        Operation add = new Add(tmp);
+        add.execute();
+        
+        variablesMap.replace(var, tmp.pop());
+    }
+    
+    /**
+     * Metodo statico per sottrarrelal valore del numero complesso in cima allo stack al valore
+     * della variabile data
+     * 
+     * @param stack Stack di numeri complessi
+     * @param var Variabile il cui valore deve essere sottratto al numero complesso in cima allo stack
+     */
+    public void variableSubtract(Stack<ComplexNumber> stack, String var) throws NotSelectedVariableException{
+        if(var==null)
+           throw new NotSelectedVariableException();
+        if(variablesMap.get(var) == null)
+            throw new NotSelectedVariableException("La variabile non contiene alcun valore.\nInizializzarla prima");
+        
+        Stack<ComplexNumber> tmp = new Stack<>();
+        tmp.push(variablesMap.get(var));
+        tmp.push(stack.peek());
+        
+        
+        Operation sub = new Sub(tmp);
+        sub.execute();
+        
+        variablesMap.replace(var, tmp.pop());        
+    }
+    
     /**
      * Ritorna una stringa che rappresenta l' HashMap.
      * @return la sequenza di elementi visti come coppia chiave-valore contenuti nell'HashMap 
