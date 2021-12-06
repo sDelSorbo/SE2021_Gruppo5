b9193dcc2692121ef5dbfce7eab2e84f692c425b
gianpaolotobia <gianpaolotobia@mbpdigianpaolo.homenet.telecomitalia.it>
2021-12-06 20:23:08 +0100

    Adding serialized saving operations

--- SE2021_Gruppo5/src/it/unisa/diem/se/group5/calculator/gui/FXMLDocumentController.java
+++ SE2021_Gruppo5/src/it/unisa/diem/se/group5/calculator/gui/FXMLDocumentController.java
@@ -11,11 +11,14 @@ import it.unisa.diem.se.group5.calculator.complex.userdefinedoperations.Malforme
 import it.unisa.diem.se.group5.calculator.complex.userdefinedoperations.UserDefinedOperation;
 import it.unisa.diem.se.group5.calculator.complex.userdefinedoperations.UserDefinedOperations;
 import it.unisa.diem.se.group5.calculator.complex.variables.Variables;
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.Collections;
@@ -365,10 +368,13 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void saveOperations(ActionEvent event) {
     FileChooser fc = new FileChooser();
-    fc.setTitle("Salva File Operazioni");
+    fc.setTitle("Save Operations");
     File filename= fc.showSaveDialog(rootPane.getScene().getWindow());
      try (ObjectOutputStream dout = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(filename)))) {
-                dout.writeObject(userOperationsObs);
+              for(UserDefinedOperation e: userOperationsObs)  {
+                dout.writeChars(e.getName());
+                dout.writeChars(e.getOperationsString());
+              }
             } catch (FileNotFoundException ex) {
             Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -378,6 +384,23 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void openOperationsFile(ActionEvent event) {
-        
+    FileChooser fc = new FileChooser();
+    fc.setTitle("Open Operations File");
+    File file= fc.showOpenDialog(rootPane.getScene().getWindow());
+                    System.out.println("nome ");
+      try (ObjectInputStream din = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))){
+            for(UserDefinedOperation e: userOperationsObs)  {
+                String name = din.readUTF();
+                System.out.println("nome " + name);
+                String ops = din.readUTF();
+                e = new UserDefinedOperation(name,ops);
+                userOperationsObs.add(e);
+              }
+      } catch (FileNotFoundException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
     }
 }
