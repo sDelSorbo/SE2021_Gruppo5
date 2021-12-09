ec035260fecadcca39624839efd601f4393795ce
Marco <Marco@DESKTOP-VP4CKLK.mshome.net>
2021-12-09 22:48:23 +0100

    Aggiustata Pow e rifatto Test

--- SE2021_Gruppo5/src/it/unisa/diem/se/group5/calculator/strategy/SerialSaveRestore.java
+++ SE2021_Gruppo5/src/it/unisa/diem/se/group5/calculator/strategy/SerialSaveRestore.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * SerialSaveRestore
+ *
  */
 package it.unisa.diem.se.group5.calculator.strategy;
 
